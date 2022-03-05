239e142a7f75e2f7af3a34dd80f19fc173b22b5d
Afsal <afsalafzz63@gmail.com>
2022-03-05 17:24:02 +0530

    register done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,29 @@
 import React from "react";
 import MainPage from "./screens/pages/MainPage";
- import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LogginScreen from "./screens/LogginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 
 
-const App=()=> {
+const App = () => {
   return (
-    
-    <>
-    <Router>
-      
 
-      <Routes>
-     <Route path='/login' element={<LogginScreen/>}></Route>   
-<Route path='/product/:id' element={<ProductScreen/>}></Route>
-<Route path='/cart' element={<CartScreen/>}></Route>
-<Route path='/cart/:id' element={<CartScreen/>}></Route>
-<Route path='/' element={<MainPage/>}></Route>
-</Routes>
-</Router>
+    <>
+      <Router>
+
+
+        <Routes>
+          <Route path='/login' element={<LogginScreen />}></Route>
+        <Route path='/register' element={<RegisterScreen/>}></Route>
+          <Route path='/product/:id' element={<ProductScreen />}></Route>
+          <Route path='/cart' element={<CartScreen />}></Route>
+          <Route path='/cart/:id' element={<CartScreen />}></Route>
+          <Route path='/' element={<MainPage />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
