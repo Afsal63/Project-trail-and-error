0b55caf060cd95ac66e9226208f5a3d398698bfa
Afsal <afsalafzz63@gmail.com>
2022-03-05 12:33:49 +0530

    login was done success fully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import MainPage from "./screens/pages/MainPage";
  import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LogginScreen from "./screens/LogginScreen";
 
 
 
@@ -14,10 +15,11 @@ const App=()=> {
       
 
       <Routes>
-<Route path='/' element={<MainPage/>}></Route>
+     <Route path='/login' element={<LogginScreen/>}></Route>   
 <Route path='/product/:id' element={<ProductScreen/>}></Route>
 <Route path='/cart' element={<CartScreen/>}></Route>
 <Route path='/cart/:id' element={<CartScreen/>}></Route>
+<Route path='/' element={<MainPage/>}></Route>
 </Routes>
 </Router>
     </>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,11 +6,12 @@ import {
     productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-
+import {userLoginReducer} from './reducers/userReducers'
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 
@@ -18,8 +19,11 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
  ? JSON.parse(localStorage.getItem('cartItems'))
     : [];
 
+    const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const intialState = {
-    cart: { cartItems: cartItemsFromStorage }
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin:{userInfo:userInfoFromStorage}
 }
 
 const middleware = [thunk]
